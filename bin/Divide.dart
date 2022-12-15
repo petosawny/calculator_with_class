e445505645c568276d7df1ace0543ec84b382925
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-15 03:55:12 +0200

    Update Divide.dart

--- bin/Divide.dart
+++ bin/Divide.dart
@@ -20,7 +20,8 @@ class Divide extends Calculator{
     number2 =num.parse(stdin.readLineSync()!);   // insert value to the variable number2 from the extended class from user
 
 
-    print("the result of   $number1  /  $number2 is " +(number1! / number2!).toString() );
+    print("the result of   $number1  /  $number2 is " +(number1! / number2!).toString() );   // print the theme of the operation with the insertion numbers
+
 
   }
 
