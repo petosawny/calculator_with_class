7845472e2da3361dc8782ca91d7ad8b616064bf2
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-15 03:53:14 +0200

    Update Calculator.dart

--- bin/Calculator.dart
+++ bin/Calculator.dart
@@ -1,19 +1,23 @@
+// import packages and classes
 
 import 'dart:io';
+// master class
 
 class Calculator{
+  // variables
 
   num? number1 ;
   num? number2 ;
+  //void fun
 
 getNumbers(){
 
 
 print("enter the first number");
-  num? number1 =num.parse(stdin.readLineSync()!);
+  num? number1 =num.parse(stdin.readLineSync()!);      // insert value to the variable number1 from user
 
 print("enter the second number");
-  num? number2 =num.parse(stdin.readLineSync()!);
+  num? number2 =num.parse(stdin.readLineSync()!);       // insert value to the variable number2 from user
 
 }
 
