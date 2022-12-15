0693db2d8b740664214ddf2f401b2a33c48335a2
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-15 03:53:52 +0200

    Update Divide.dart

--- bin/Divide.dart
+++ bin/Divide.dart
@@ -1,19 +1,24 @@
+// import packages and classes
 
 import 'dart:io';
 
 import 'Calculator.dart';
+// class called Divide extends from the class Calculator
 
 class Divide extends Calculator{
 
 
+  // void Function print the result of the calculation
 
   div(){
 
     print("enter the first number");
-    super.number1 =num.parse(stdin.readLineSync()!);
+    number1 =num.parse(stdin.readLineSync()!);   // insert value to the variable number1 from the extended class from user
+
 
     print("enter the second number");
-    super.number2 =num.parse(stdin.readLineSync()!);
+    number2 =num.parse(stdin.readLineSync()!);   // insert value to the variable number2 from the extended class from user
+
 
     print("the result of   $number1  /  $number2 is " +(number1! / number2!).toString() );
 
