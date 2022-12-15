965766a137faa22ac5f37a18160500bc1e293bbc
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-15 03:59:51 +0200

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,35 +1,42 @@
+// import packages and classes
 import 'dart:io';
-
 import 'Divide.dart';
 import 'Subtraction.dart';
 import 'Sum.dart';
 import 'multiplication.dart';
 
 void main(List<String> arguments) {
-
+  // print statement show to the user the operations
   print(''''enter the operation
   if you want to Plus pres number       + 
   if you want to Subtract pres number   - 
   if you want to Multiply pres number   * 
   if you want to Devide pres number     / ''');
-  String? op = stdin.readLineSync();
+  String? op =
+      stdin.readLineSync(); // insert value to the variable op from user
 
-  if(op=="/") {
+// if statement to comparing the operation which the user need and calculate the operation with the relevant class
+  if (op == "/") {
+    // creating object from the class Divide
     var obj1 = Divide();
-    obj1.div();
-  }
-  else if(op=="*") {
+    obj1.div(); //calling fun with the object from the class
+  } else if (op == "*") {
+    // creating object from the class Multiply
     var obj2 = Multiply();
-    obj2.mul();
-  }
-  else if(op=="-") {
+    obj2.mul(); //calling fun with the object from the class
+
+  } else if (op == "-") {
+    // creating object from the class Subtract
     var obj3 = Subtract();
-    obj3.sub();
-  }
-  else if(op=="+") {
+    obj3.sub(); //calling fun with the object from the class
+
+  } else if (op == "+") {
+    // creating object from the class Sum
     var obj4 = Sum();
-    obj4.sum();
+    obj4.sum(); //calling fun with the object from the class
+
   } else {
-    print("enter a valid operator");
+    // if the user insert an input out of choices print notation statement
+    print("enter a valid operator"); // notation message
   }
 }
